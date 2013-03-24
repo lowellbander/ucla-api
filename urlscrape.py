246e6ffec73eeb44fb58907e00abbb6ecf801c46
shazeline <shazeline@gmail.com>
2013-03-23 19:41:07 -0700

    fix index bugs and refactor

--- urlscrape.py
+++ urlscrape.py
@@ -2,18 +2,19 @@ import re
 import urllib2
 from bs4 import BeautifulSoup
 
-class Department:    
+class Department:
     def __init__(self, c, n):
         self.code = c
         self.name = n
 
-class Course:    
-    def __init__(self, c, n):
+class Course:
+    def __init__(self, c, n, u):
         self.code = c
         self.name = n
+        self.url = u
 
 class Listing:
-    def __init__(self, idnum, ctype, sec, days, start, stop, build, room, rest, en, encap, wait, waitcap, status):
+    def __init__(self, idnum, ctype, sec, days, start, stop, build, room, rest, en, encap, wait, waitcap, status,u):
         self.id = idnum
         self.type = ctype
         self.section = sec
@@ -28,80 +29,35 @@ class Listing:
         self.waitlisttotal = wait
         self.waitlistcapacity = waitcap
         self.status = status
-
+        self.url = u
 
 #pulls data from a class url
 #for example: http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=13S&subareasel=PHYSICS&idxcrs=0004AL++
-def get_listing_data(dept,course):
+def get_listings(dept,course):
     #creates a BSoup object with parse tree from url
     base = "http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=13S&subareasel="+dept+"&idxcrs="+course
-    base = base.replace(' ', '%20')            
+    base = base.replace(' ', '%20')
     opener = urllib2.build_opener()
     url_opener = opener.open(base)
     page = url_opener.read()
-    soup = BeautifulSoup(page) 
+    soup = BeautifulSoup(page)
 
-    #scrapes across the registrar table
-    idnumber = []
-    for elem in soup.find_all("span", id=re.compile("lblIDNumber")):
-        idnumber.append(elem.text.strip())
+    tags = ["lblIDNumber","lblActType","lblSectionNumber","lblDays","lblTimeStart","lblTimeEnd","lblBuilding","lblRoom","Restrict","EnrollTotal","EnrollCap","WaitListTotal","WaitListCap","Status"]
+    table = []    
+    for i in range(len(tags)):
+        tag = tags[i]
+        subtable = []
+        for elem in soup.find_all("span", id=re.compile(tag)):
+            subtable.append(elem.text.strip()) 
+        table.append(subtable)
+    # for i in range(len(table)):
+    #     print table[i]
+    #     print "\n"    
 
-    acttype = []
-    for elem in soup.find_all("span", id=re.compile("lblActType")):
-        acttype.append(elem.text.strip())
-
-    days = []
-    for elem in soup.find_all("span", id=re.compile("lblDays")):
-        days.append(elem.text.strip())
-
-    secnum = []
-    for elem in soup.find_all("span", id=re.compile("lblSectionNumber")):
-        secnum.append(elem.text.strip())
-
-    tstart = []
-    for elem in soup.find_all("span", id=re.compile("lblTimeStart")):
-        tstart.append(elem.text.strip())
-
-    tend = []
-    for elem in soup.find_all("span", id=re.compile("lblTimeEnd")):
-        tend.append(elem.text.strip())
-
-    building = []
-    for elem in soup.find_all("span", id=re.compile("lblBuilding")):
-        building.append(elem.text.strip())
-
-    room = []
-    for elem in soup.find_all("span", id=re.compile("lblRoom")):
-        room.append(elem.text.strip())
-
-    rest = []
-    for elem in soup.find_all("span", id=re.compile("Restrict")):
-        rest.append(elem.text.strip())
-
-    etotal = []
-    for elem in soup.find_all("span", id=re.compile("EnrollTotal")):
-        etotal.append(elem.text.strip())
-
-    ecap = []
-    for elem in soup.find_all("span", id=re.compile("EnrollCap")):
-        ecap.append(elem.text.strip())
-
-    wtotal = []
-    for elem in soup.find_all("span", id=re.compile("WaitListTotal")):
-        wtotal.append(elem.text.strip())
-
-    wcap = []
-    for elem in soup.find_all("span", id=re.compile("WaitListCap")):
-        wcap.append(elem.text.strip())
-
-    status = []
-    for elem in soup.find_all("span", id=re.compile("Status")):
-        status.append(elem.text.rstrip("\n").strip())
-
-    listings = []
-    for i in range(len(idnumber)):
-        listing = Listing(idnumber[i],acttype[i],days[i],secnum[i],tstart[i],tend[i],building[i],room[i],rest[i],etotal[i],ecap[i],wtotal[i],wcap[i],status[i])
-        listings.append(listing)
+    listings = []    
+    for i in range(len(table[0])):
+        listing = Listing(table[0][i],table[1][i],table[2][i],table[3][i],table[4][i],table[5][i],table[6][i],table[7][i],table[8][i],table[9][i],table[10][i],table[11][i],table[12][i],table[13][i],base)
+        listings.append(listing)    
     return listings
 
 #returns list of all departments
@@ -111,15 +67,15 @@ def get_depts():
     opener = urllib2.build_opener()
     url_opener = opener.open(base)
     page = url_opener.read()
-    soup = BeautifulSoup(page) 
-    
+    soup = BeautifulSoup(page)
+
     #adds all department codes to array (ex: COM SCI)
     codes = []
     for elem in soup.find_all("option"):
-        codes.append(elem['value'].strip())           
-   
+        codes.append(elem['value'].strip())
+
    #adds all department names to array (ex: Computer Science)
-    names = []    
+    names = []
     for elem in soup.find_all("option"):
         names.append(elem.text.strip())
 
@@ -138,6 +94,7 @@ def get_depts():
 def get_courses(dept):
     #creates a BSoup object with parse tree from url
     base = "http://www.registrar.ucla.edu/schedule/crsredir.aspx?termsel=13S&subareasel="+dept
+    base = base.replace(' ', '%20')       
     opener = urllib2.build_opener()
     url_opener = opener.open(base)
     page = url_opener.read()
@@ -146,10 +103,10 @@ def get_courses(dept):
     #adds all department codes to array (ex: COM SCI)
     codes = []
     for elem in soup.find_all("option"):
-        codes.append(elem['value'].strip())           
-   
+        codes.append(elem['value'].strip())
+
    #adds all department names to array (ex: Computer Science)
-    names = []    
+    names = []
     for elem in soup.find_all("option"):
         names.append(elem.text.strip())
 
@@ -159,20 +116,39 @@ def get_courses(dept):
 
     courses = []
     for i in range(len(codes)):
-        course = Course(codes[i],names[i])
+        course = Course(codes[i],names[i],base)
         courses.append(course)
 
     return courses
 
+def test_listing():
+    test = get_listings("COM SCI", "0031")     
+    for i in range(len(test)):
+        print test[i].id
+        print test[i].type
+        print test[i].section
+        print test[i].days
+        print test[i].starttime
+        print test[i].stoptime
+        print test[i].building
+        print test[i].room
+        print test[i].restricted
+        print test[i].enrollmenttotal
+        print test[i].enrollmentcapacity
+        print test[i].waitlisttotal
+        print test[i].waitlistcapacity
+        print test[i].status
+        print test[i].url
+        print "\n"
 
+def test_all():
+    depts = get_depts()
+    for dept in depts:
+        courses = get_courses(dept.code)
+        for course in courses:
+            listings = get_listings(dept.code,course.code)
+            print course.name
+            for listing in listings:
+                print listing.type + " " + listing.section + " " + listing.status
 
-depts = get_depts()
-dept = depts[4].code
-print dept
-
-courses = get_courses(dept)
-course = courses[0].code
-print courses[0].name
-
-data = get_listing_data(dept,course)
-print data[0].room
+test_listing()
